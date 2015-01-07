b7846e508e719473ea9c60fb50493d32105535a9
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2015-01-07 14:20:28 +0000

    Fix bug in determining BH threshold: BH is a step-up procedure!

--- R/helpers.R
+++ R/helpers.R
@@ -24,5 +24,5 @@ get_bh_threshold <- function(pvals, alpha, mtests = length(pvals)){
   m <- length(pvals)
   pvals <- sort(pvals)
   prejected <- which(pvals <= (1:m)/mtests*alpha)
-  ifelse(length(prejected)==0, 0, pvals[which.max(prejected)])
+  ifelse(length(prejected)==0, 0, pvals[prejected[which.max(prejected)]])
 }
