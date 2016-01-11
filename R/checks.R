31c3bdfd90cadc0cd62524d3e5f2d3bcfb2cc43e
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2016-01-11 14:58:22 +0100

    try to get travis running with bioc devel
    2nd attempt at travis bioc devel
    
    3rd attempt..
    
    4th attempt

--- R/checks.R
+++
@@ -1,6 +0,0 @@
-# small suite of functions to check if input from user is correct etc.
-# also useful for unit tests!
-
-check_uniform_groups <- function(groups){
-	diff(range(table(groups))) <= 1
-}
--- tests/testthat/test_grouping.R
+++ tests/testthat/test_grouping.R
@@ -1,4 +1,8 @@
 test_that("number of elements in each group should differ by at most 1", {
+    check_uniform_groups <- function(groups){
+        diff(range(table(groups))) <= 1
+    }
+
  	groups <- groups_by_filter(runif(1000),7)
 	expect_true(check_uniform_groups(groups))
 })
